f7b84ed52a9be7d04161b541efb3ad748390fd34
Chen <42896084+chentian86@users.noreply.github.com>
2022-11-30 22:58:10 +0000

    Update 35. Search Insert Position.py

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -1,15 +1,15 @@
 # Problem 35. Search Insert Position
 # Link: https://leetcode.com/problems/search-insert-position/
 class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        if sum(nums[1:])==0:
-            return 0
-        i=0
-        while i < len(nums):
-            if sum(nums[0:i])==sum(nums[i+1:]):
-                return i
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        while left<=right:
+            mid = int((left+right)/2)
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
             else:
-                i+=1
-        if sum(nums[:-1])==0:
-            return len(nums)-1
-        return -1
+                right=mid-1
+        return left
